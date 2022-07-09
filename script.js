5791dbddab60b449d5c9dad8565898ebc0a7805a
9812598 <9812598@gmail.com>
2022-07-09 20:12:29 +0300

    Rock Papar Scissors

--- script.js
+++ script.js
@@ -18,30 +18,30 @@ function  playerSelect() {
 }
 
 
-let layR
-function playRound(computerSelection, playerSelection){
-    let res
+let roundResult
+function playRound(){
+
     playerSelect();
     compSelect();
 
-    if (computerSelection == playerSelection) res = `eqval`
+    if (computerSelection == playerSelection) roundResult = `eqval`
     else {
         switch (playerSelection) {
         case 'rock':
-            (computerSelection == 'paper') ? res = 'win': res = 'lose';
+            (computerSelection == 'paper') ? roundResult = 'lose': roundResult = 'win';
             break;
         case 'paper':
-            (computerSelection == 'scissors') ? res = 'win': res = 'lose';
+            (computerSelection == 'scissors') ? roundResult = 'lose': roundResult = 'win';
             break;
         case 'scissors':
-            (computerSelection == 'paper') ? res = 'win': res = 'lose';
+            (computerSelection == 'paper') ? roundResult = 'win': roundResult = 'lose';
             break;
         }
         // massage
-        if (res == 'eqval') console.log(`Draw. Try again. You both chose ${computerSelection}`)
-        else if (playRound == 'win') console.log(`You win. Computer has ${computerSelection}`)
-        else console.log(`You lose. Computer has ${computerSelection}`)
-        layR = res;
+    if (roundResult == 'eqval') console.log(`Draw. Try again. You both chose ${computerSelection}`)
+    else if (roundResult == 'win') console.log(`You win. Computer has ${computerSelection}`)
+    else console.log(`You lose. Computer has ${computerSelection}`)
+    
 }
 }
 
@@ -51,15 +51,25 @@ function game(){
     let compWinCount = 0;
     let playerWinCount = 0;
     for (let i = 0; i < 5; i++) {
-        playRound(computerSelection, playerSelection);
+        playRound();
 
-        if (layR == 'win') playerWinCount += 1;
-        else if (layR == 'lose') compWinCount += 1;
+        if (roundResult == 'win') {
+            playerWinCount += 1;
+            console.log(`you win ${i} round`)
+        }
+        else if (roundResult == 'lose') {
+            compWinCount += 1;
+            console.log(`you lose ${i} round`)
+        }
+        else if (roundResult == 'eqval') {
+            console.log(`${i} round eqval`)
+        }
+
      }
-    (playerWinCount < compWinCount) ? console.log(`You lose. Computer wins ${compWinCount} times`): 
-    (playerWinCount == compWinCount) ? console.log(`You have same wins as computer. Computer wins ${compWinCount} times`): 
-    console.log(`You win. Computer wins only ${compWinCount} times`);
-};
+    (playerWinCount < compWinCount) ? console.log(`You lose. Computer wins ${compWinCount} / ${playerWinCount}`): 
+    (playerWinCount == compWinCount) ? console.log(`You have same wins as computer.  Wins ${compWinCount} / ${playerWinCount} `): 
+    console.log(`You win. Computer wins only ${compWinCount} / ${playerWinCount}`);
+}
 
 
 game();
